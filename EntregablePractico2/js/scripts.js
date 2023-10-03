72e3e6a35b35112f6cfe6835f053264de8c978ab
Lucas Quiroga <quiroga9409@gmail.com>
2023-10-03 19:55:34 -0300

    Implemented category dropdown menu

--- EntregablePractico2/js/scripts.js
+++ EntregablePractico2/js/scripts.js
@@ -1,5 +1,7 @@
 const userIcon = document.getElementById('user-icon');
 const userDropdown = document.getElementById('user-dropdown');
+const menuIcon = document.getElementById("menu-icon");
+const menuDropdown = document.getElementById("menu-dropdown");
 
 userIcon.addEventListener('click', function () {
     if (userDropdown.style.display === 'block') {
@@ -13,5 +15,21 @@ userIcon.addEventListener('click', function () {
 document.addEventListener('click', function (event) {
     if (!userIcon.contains(event.target) && !userDropdown.contains(event.target)) {
         userDropdown.style.display = 'none';
+    }
+});
+
+menuIcon.addEventListener("click", () => {
+    // Alterna la visibilidad del menú al hacer clic en el icono
+    if (menuDropdown.style.display === "block") {
+        menuDropdown.style.display = "none";
+    } else {
+        menuDropdown.style.display = "block";
+    }
+});
+
+// Agrega un event listener para ocultar el menú si se hace clic en cualquier otro lugar del documento
+document.addEventListener("click", (event) => {
+    if (event.target !== menuIcon && event.target !== menuDropdown) {
+        menuDropdown.style.display = "none";
     }
 });
