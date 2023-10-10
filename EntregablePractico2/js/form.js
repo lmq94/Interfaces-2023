bf9d412eeb8a9cebf462991be049981a098bf881
Lucas Quiroga <quiroga9409@gmail.com>
2023-10-09 21:43:29 -0300

    Refactor htmls

--- EntregablePractico2/js/form.js
+++ EntregablePractico2/js/form.js
@@ -1,1 +1,24 @@
 "use strict"
+document.addEventListener('DOMContentLoaded', () => {
+    const signUpButton = document.getElementById('signUp');
+    const successMessage = document.getElementById('successMessage');
+    const form = document.getElementById('register'); // Obtén el formulario
+
+    signUpButton.addEventListener('click', (event) => {
+        event.preventDefault(); // Evita que el formulario se envíe (si es un formulario)
+
+        // Realiza la validación de los campos requeridos
+        if (form.checkValidity()) {
+            // Muestra el mensaje de registro exitoso y el enlace
+            successMessage.style.display = 'block';
+
+            // Oculta el mensaje después de 5 segundos (5000 milisegundos)
+            setTimeout(() => {
+                successMessage.style.display = 'none';
+            }, 5000);
+        } else {
+            // Si no se cumple la validación, muestra un mensaje de error o realiza alguna otra acción
+            alert('Por favor, complete todos los campos requeridos.');
+        }
+    });
+});    
