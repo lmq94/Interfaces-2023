045a15f4dcf9a23d333e119083eda7fadea7b781
Lucas Duana <lucasd-mdp@hotmail.com.ar>
2023-10-24 12:34:12 -0300

    cambios reentrega

--- EntregablePractico2/js/form.js
+++ EntregablePractico2/js/form.js
@@ -1,1 +1,32 @@
-"use strict"
+"use strict";
+document.addEventListener('DOMContentLoaded', function() {
+    let form = document.querySelector('#register');
+
+    form.addEventListener('submit', validar);
+
+    function validar(e) {
+        e.preventDefault();
+        let formularioData = new FormData(form);
+        let nameInput = formularioData.get('name');
+        let ageInput = formularioData.get('age');
+        let emailInput = formularioData.get('email');
+        let password1Input = formularioData.get('password1');
+        let password2Input = formularioData.get('password2');
+
+        console.log(nameInput);
+        console.log(ageInput);
+        console.log(emailInput);
+        console.log(password1Input);
+        console.log(password2Input);
+    
+        if (password1Input!=password2Input){
+            let inputp1=document.querySelector('input[name="password1"]');
+            let inputp2=document.querySelector('input[name="password2"]');
+            inputp1.classList.toggle('error');
+            inputp2.classList.toggle('error');
+            let error=document.querySelector('input[name="password2"]').nextElementSibling;
+            error.innerHTML="Las claves no coinciden";
+        }  
+        
+    }
+});
